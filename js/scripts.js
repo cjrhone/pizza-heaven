2add38d736eb7bf0666d52bd3498bc900c033bf7
CJ Rhone <charlesrhonejr@gmail.com>
2018-04-06 13:00:16 -0700

    Add name and email input, and empty object for customer

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,50 @@
+
+//Business Logic
+var finalCost = 0;
+
+function customer(name,email,pizza,cost) {
+  this.name = name;
+  this.email = email;
+  this.pizza = pizza;
+  this.cost = cost
+}
+
+function pizza(size, toppings, delivery) {
+  this.size = size;
+  this.toppings = toppings;
+  this.delivery = delivery;
+}
+
+customer.addCost = function() {
+  return this.cost + 1
+};
+
+var newCustomer = new customer(inputtedName, inputtedEmail);
+var inputtedName = $("input-name").val();
+var inputtedEmail = $("input-email").val();
+
+
+
+
+
+//User Logic
 $(document).ready(function() {
 
   $("#pizza-form").submit(function(event) {
     event.preventDefault();
 
-    alert("you clicked a button!");
+    $("input:checkbox[name=toppings]:checked").each(function(){
+      finalCost++; // increases +1 per topping to final cost
+    });
+
+    var inputtedName = $(".input-name").val();
+    var inputtedEmail = $(".input-email").val();
+    var newCustomer = new customer(inputtedName, inputtedEmail);
+
+console.log(finalCost);
+console.log(newCustomer);
+console.log(inputtedName);
+console.log(inputtedEmail);
 
     $(".purchase-results").show();
 
